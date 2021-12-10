3a0c13447be4797e0b7b89defb44837e933fe6b7
Bosco Kwong <95505913+boscokwong@users.noreply.github.com>
2021-12-10 17:44:19 -0600

    Update index.html.js

--- index.html.js
+++ index.html.js
@@ -24,4 +24,36 @@
                 button1 = new button();
                 
             };
-            document.cookie = "weblink1=; expires=Sat, 31 Dec 2022 23:59:59 UTC";
+            function setCookie(cname, cvalues, exdays) {
+                const d = new date();
+                d.setTime(d.getTime() +
+                (exdays*24*60*60*1000));
+                let expires = "expires="+ d.toUTCString();
+                document.cookie = cname + "=" + cvalue + ";" + expires;
+            }
+            function getCookie(cname) {
+              let name = cname + "=";
+              let decodedCookie = decodeURIComponent(document.cookie);
+              let ca = decodedCookie.split(';');
+              for(let i = 0; i <ca.length; i++) {
+                let c = ca[i];
+                while (c.charAt(0) == ' ') {
+                  c = c.substring(1);
+                }
+                if (c.indexOf(name) == 0) {
+                  return c.substring(name.length, c.length);
+                }
+              }
+              return "";
+            }
+            function checkCookie() {
+              let username = getCookie("username");
+              if (username != "") {
+               alert("Welcome again " + username);
+              } else {
+                username = prompt("Please enter your name:", "");
+                if (username != "" && username != null) {
+                  setCookie("username", username, 365);
+                }
+              }
+            }
