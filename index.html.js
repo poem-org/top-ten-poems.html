65905e2b1d2978cbce4de03dcaaa4e274cb9edcc
Bosco Kwong <95505913+boscokwong@users.noreply.github.com>
2021-12-10 16:51:14 -0600

    Create index.html.js

---
+++ index.html.js
@@ -0,0 +1,26 @@
+/**
+            REGULAR ProcessingJs BELOW(DOM Too) 
+            **/
+            var nameEls1 = document.GetElementById (
+                "#poem_2_part_1"
+            );
+            var nameEls2 = document.GetElementById(
+                "#poem_2_part_2"
+            );
+            var nameEls3 = document.GetElementById(
+                "#III"
+            );
+            var nameEls4 = document.GetElementById(
+                "#IV"
+            );
+            var nameEls5 = document.GetElementById(
+                "#V"
+            );
+            var button1_text
+            button1_text.addClassName = ("prototype");
+            var button1
+            button1.addClassName = ("button");
+            var mouseClicked = function() {
+                button1 = new button();
+                
+            };
