4edfdf07a71e868834b81de7f1df59bdd8cafe83
Bosco Kwong <95505913+boscokwong@users.noreply.github.com>
2021-12-10 18:15:36 -0600

    Update index.html.js

--- index.html.js
+++ index.html.js
@@ -24,18 +24,18 @@
                 button1 = new button();
                 
             };
-            function setCookie(cname, cvalues, exdays) {
-                const d = new date();
-                d.setTime(d.getTime() +
-                (exdays*24*60*60*1000));
-                let expires = "expires="+ d.toUTCString();
-                document.cookie = cname + "=" + cvalue + ";" + expires;
+            function setCookie(cname,cvalue,exdays) {
+              const d = new Date();
+              d.setTime(d.getTime() + (exdays*24*60*60*1000));
+              let expires = "expires=" + d.toGMTString();
+              document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
             }
+
             function getCookie(cname) {
               let name = cname + "=";
               let decodedCookie = decodeURIComponent(document.cookie);
               let ca = decodedCookie.split(';');
-              for(let i = 0; i <ca.length; i++) {
+              for(let i = 0; i < ca.length; i++) {
                 let c = ca[i];
                 while (c.charAt(0) == ' ') {
                   c = c.substring(1);
@@ -46,14 +46,15 @@
               }
               return "";
             }
+
             function checkCookie() {
-              let username = getCookie("username");
-              if (username != "") {
-               alert("Welcome again " + username);
+              let user = getCookie("username");
+              if (user != "") {
+                alert("Welcome again " + user);
               } else {
-                username = prompt("Please enter your name:", "");
-                if (username != "" && username != null) {
-                  setCookie("username", username, 365);
-                }
+                 user = prompt("Please enter your name:","");
+                 if (user != "" && user != null) {
+                   setCookie("username", user, 30);
+                 }
               }
             }
