cf50c17b77457caa0ea865078d72ac715fb7d9b5
Bosco Kwong <95505913+boscokwong@users.noreply.github.com>
2021-12-16 15:48:02 -0600

    Update index.html.js

--- index.html.js
+++ index.html.js
@@ -24,19 +24,20 @@
                 button1 = new button();
                 
             };
-            function setCookie(cname,cvalue,exdays) {
-              const d = new Date();
-              d.setTime(d.getTime() + (exdays*24*60*60*1000));
-              let expires = "expires=" + d.toGMTString();
-              document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+               
+            function setCookie(cname, cvalues, exdays) {
+                const d = new date();
+                d.setTime(d.getTime() +
+                (exdays*24*60*60*1000));
+                var expires = "expires="+ d.toUTCString();
+                document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
             }
-
             function getCookie(cname) {
-              let name = cname + "=";
-              let decodedCookie = decodeURIComponent(document.cookie);
-              let ca = decodedCookie.split(';');
-              for(let i = 0; i < ca.length; i++) {
-                let c = ca[i];
+              var name = cname + "=";
+              var decodedCookie = decodeURIComponent(document.cookie);
+              var ca = decodedCookie.split(';');
+              for(var i = 0; i <ca.length; i++) {
+                var c = ca[i];
                 while (c.charAt(0) == ' ') {
                   c = c.substring(1);
                 }
@@ -46,15 +47,25 @@
               }
               return "";
             }
-
             function checkCookie() {
-              let user = getCookie("username");
-              if (user != "") {
-                alert("Welcome again " + user);
+              var username = getCookie("username");
+              if (username != "") {
+               alert("Welcome again " + username);
               } else {
-                 user = prompt("Please enter your name:","");
-                 if (user != "" && user != null) {
-                   setCookie("username", user, 30);
-                 }
+                username = prompt("Please enter your name:", "");
+                if (username != "" && username != null) {
+                  setCookie("username", username, 365);
+                }
               }
             }
+            function WriteCookie() {
+                var now = new Date();
+                now.setMonth( now.getMonth() + 12 );
+                var domain = "boscokwong.github.io;";
+                
+                cookievalue = "GAI_1hjkl;"
+               
+                document.cookie = "name=" + cookievalue;
+                document.cookie = "expires=" + now + ";";
+                document.cookie = "domain=" + domain;
+                
